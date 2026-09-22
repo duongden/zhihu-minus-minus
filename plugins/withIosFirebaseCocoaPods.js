7ce0887410839a3950d77b2bff9bbb56cb484285
huamurui <huamurui@outlook.com>
2026-09-22 18:43:29 +0800

    ios15.1

--- plugins/withIosFirebaseCocoaPods.js
+++ plugins/withIosFirebaseCocoaPods.js
@@ -6,6 +6,16 @@ const {
 const TAG = '@react-native-firebase/app-disableSPM';
 const ANCHOR = /prepare_react_native_project!/;
 const FLAG = '$RNFirebaseDisableSPM = true';
+const DEPLOYMENT_TARGET_TAG = 'ios-pods-minimum-deployment-target';
+const POST_INSTALL_ANCHOR = /post_install do \|installer\|/;
+const MINIMUM_DEPLOYMENT_TARGET = `installer.pods_project.targets.each do |pod_target|
+      pod_target.build_configurations.each do |build_config|
+        deployment_target = build_config.build_settings['IPHONEOS_DEPLOYMENT_TARGET']
+        if deployment_target && Gem::Version.new(deployment_target) < Gem::Version.new('15.1')
+          build_config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1'
+        end
+      end
+    end`;
 
 module.exports = function withIosFirebaseCocoaPods(config) {
   return withPodfile(config, (podfileConfig) => {
@@ -14,6 +24,14 @@ module.exports = function withIosFirebaseCocoaPods(config) {
       newSrc: FLAG,
       tag: TAG,
       anchor: ANCHOR,
+      offset: 1,
+      comment: '#',
+    }).contents;
+    podfileConfig.modResults.contents = mergeContents({
+      src: podfileConfig.modResults.contents,
+      newSrc: MINIMUM_DEPLOYMENT_TARGET,
+      tag: DEPLOYMENT_TARGET_TAG,
+      anchor: POST_INSTALL_ANCHOR,
       offset: 1,
       comment: '#',
     }).contents;
